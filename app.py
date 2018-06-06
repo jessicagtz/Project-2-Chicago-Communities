5b9ac2351aaa54d3a78c6ab9b5a4798ba4905c34
Madeleine Cieslak <YanagaM@fxw.org>
2018-06-06 16:17:20 -0500

    updated files

--- app.py
+++ app.py
@@ -33,6 +33,8 @@ Base.prepare(engine, reflect=True)
 Communities = Base.classes.comm_names
 Neighborhoods = Base.classes.neighborhoods
 Twitter = Base.classes.twitter
+Population = Base.classes.population
+Race = Base.classes.race
 
 # Create our session (link) from Python to the DB
 session = Session(engine)
@@ -43,6 +45,7 @@ session = Session(engine)
 
 @app.route("/dash/<ID>")
 def dash(ID):
+    # query for the community name based off of ID selected
     names = session.query(Communities).filter(Communities.id == ID)
     all_names = []
     for name in names:
@@ -50,6 +53,7 @@ def dash(ID):
         comm_dict["name"] = name.community
         all_names.append(comm_dict)
     name = jsonify(all_names)
+    # query for community twitter handle based off of ID
     twitters = session.query(Twitter).filter(Twitter.id == ID)
     handles = []
     for handle in twitters:
@@ -57,7 +61,68 @@ def dash(ID):
         handle_dict["handle"] = handle.twitter_handle
         handles.append(handle_dict)
     twitter_handle = jsonify(handles)
-    return render_template("index.html", comm_dict=comm_dict, handle_dict=handle_dict)
+    # query the population data for chart based off of ID
+    results = session.query(Population).filter(Population.id == ID)
+    totals = session.query(Population).filter(Population.id == 78)
+
+    pop = []
+    for population in results:
+        pop_dict = {}
+        pop_dict["ID"] = population.id
+        pop_dict["_1930"] = population._1930
+        pop_dict["_1940"] = population._1940
+        pop_dict["_1950"] = population._1950
+        pop_dict["_1960"] = population._1960
+        pop_dict["_1970"] = population._1970
+        pop_dict["_1980"] = population._1980
+        pop_dict["_1990"] = population._1990
+        pop_dict["_2000"] = population._2000
+        pop_dict["_2010"] = population._2010
+        pop_dict["_2015"] = population._2015    
+    
+    for total in totals:
+        pop_dict["all_1930"] = total._1930
+        pop_dict["all_1940"] = total._1940
+        pop_dict["all_1950"] = total._1950
+        pop_dict["all_1960"] = total._1960
+        pop_dict["all_1970"] = total._1970
+        pop_dict["all_1980"] = total._1980
+        pop_dict["all_1990"] = total._1990
+        pop_dict["all_2000"] = total._2000
+        pop_dict["all_2010"] = total._2010
+        pop_dict["all_2015"] = total._2015
+        pop.append(pop_dict)
+
+    population_data = jsonify(pop)
+    
+    # query the race data for chart based off of ID
+    demographics = session.query(Race).filter(Race.id == ID)
+
+    race = []
+    for demo in demographics:
+        demo_dict = {}
+        demo_dict["ID"] = demo.id
+        demo_dict["asian2015"] = demo.asian2015
+        demo_dict["black2015"] = demo.black2015
+        demo_dict["hispanic2015"] = demo.hispanic2015
+        demo_dict["other2015"] = demo.other2015
+        demo_dict["white2015"] = demo.white2015
+        race.append(demo_dict)
+    race_data = jsonify(race)
+
+    # query the neighborhood data for chart based off of ID
+    neighborhoods = session.query(Neighborhoods).filter(Neighborhoods.ID ==ID)
+
+    all_neighborhoods = []
+    for hood in neighborhoods:
+        hood_dict = {}
+        hood_dict["ID"] = hood.ID
+        hood_dict["Neighborhoods"] = hood.neighborhoods
+        all_neighborhoods.append(hood_dict)
+    neighborhood_data = jsonify(all_neighborhoods)
+    
+    # render the template
+    return render_template("index.html", comm_dict=comm_dict, handle_dict=handle_dict, pop_dict=pop_dict, demo_dict=demo_dict, hood_dict=hood_dict)
 
 
 @app.route("/twitter/<ID>")
@@ -97,6 +162,60 @@ def hoods(ID):
         all_neighborhoods.append(hood_dict)
 
     return jsonify(all_neighborhoods)
+
+@app.route("/pop/<ID>")
+def pop(ID):
+    results = session.query(Population).filter(Population.id == ID)
+    totals = session.query(Population).filter(Population.id == 78)
+
+    pop = []
+    for population in results:
+        pop_dict = {}
+        pop_dict["ID"] = population.id
+        pop_dict["1930"] = population._1930
+        pop_dict["1940"] = population._1940
+        pop_dict["1950"] = population._1950
+        pop_dict["1960"] = population._1960
+        pop_dict["1970"] = population._1970
+        pop_dict["1980"] = population._1980
+        pop_dict["1990"] = population._1990
+        pop_dict["2000"] = population._2000
+        pop_dict["2010"] = population._2010
+        pop_dict["2015"] = population._2015
+        
+    
+    for total in totals:
+        pop_dict["all_1930"] = total._1930
+        pop_dict["all_1940"] = total._1940
+        pop_dict["all_1950"] = total._1950
+        pop_dict["all_1960"] = total._1960
+        pop_dict["all_1970"] = total._1970
+        pop_dict["all_1980"] = total._1980
+        pop_dict["all_1990"] = total._1990
+        pop_dict["all_2000"] = total._2000
+        pop_dict["all_2010"] = total._2010
+        pop_dict["all_2015"] = total._2015
+        pop.append(pop_dict)
+    
+
+    return jsonify(pop)
+
+@app.route("/race/<ID>")
+def race(ID):
+    demographics = session.query(Race).filter(Race.id == ID)
+
+    race = []
+    for demo in demographics:
+        demo_dict = {}
+        demo_dict["ID"] = demo.id
+        demo_dict["asian2015"] = demo.asian2015
+        demo_dict["black2015"] = demo.black2015
+        demo_dict["hispanic2015"] = demo.hispanic2015
+        demo_dict["other2015"] = demo.other2015
+        demo_dict["white2015"] = demo.white2015
+        race.append(demo_dict)
+    
+    return jsonify(race)
 
 
 if __name__ == '__main__':
